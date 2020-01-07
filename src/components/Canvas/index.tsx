44f6cd26ac6981317ac7d2e036dbe041c7687dd1
Jang Haemin <io@jhaemin.com>
2020-01-07 11:27:19 -0500

    feat: update Canvas

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,17 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import Text from './Text'
-import './Canvas.scss'
+import React, { useState } from 'react'
+import Text from 'components/slide-objects/Text'
+import './style.scss'
+import { Slide, SlideTextObject } from 'types'
 
 type CanvasProps = {
-  currentSlide: any
+  currentSlide: Slide
 }
 
 const Canvas: React.FC<CanvasProps> = ({ currentSlide }) => {
-  const elems = currentSlide.objects.map((item: any, index: number) => {
-    if (item.type === 'text') {
-      return <div className="text">{item.data}</div>
+  const [
+    selectedSlideObject,
+    setSelectedSlideObject
+  ] = useState<HTMLElement | null>(null)
+
+  // Event listener delegation to all of the slide objects
+  window.addEventListener('pointerdown', e => {
+    const target = e.target
+    if (!(target instanceof HTMLElement)) return
+
+    const slideObject = target.closest('.slide-object') as HTMLElement
+    if (slideObject) {
+      if (slideObject.classList.contains('selected')) {
+        slideObject.classList.add('editable')
+      } else {
+        setSelectedSlideObject(slideObject)
+        slideObject.classList.add('selected')
+      }
     } else {
-      return
+      if (selectedSlideObject) {
+        selectedSlideObject.classList.remove('selected')
+      }
+    }
+  })
+
+  // Double click event listener
+  window.addEventListener('dblclick', e => {
+    const target = e.target
+    if (!(target instanceof HTMLElement)) return
+
+    const slideObject = target.closest('.slide-object') as HTMLElement
+    if (!slideObject) return
+
+    if (slideObject.classList.contains('text')) {
+      slideObject.contentEditable = 'true'
+      slideObject.classList.add('editable')
+    }
+  })
+
+  const elems = currentSlide.objects.map((item, index: number) => {
+    if (item.type === 'text') {
+      return <Text data={item as SlideTextObject} key={index} />
+    } else {
+      return <></>
     }
   })
 
