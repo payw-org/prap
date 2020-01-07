446446abd0b17942d50aa2cfd8068592a871ed2a
Jang Haemin <io@jhaemin.com>
2020-01-06 23:29:53 -0500

    types: add general file types

---
+++ src/types/index.d.ts
@@ -0,0 +1,26 @@
+export type SlideObject = {
+  type: string
+  pos: {
+    x: number
+    y: number
+  }
+}
+
+export type SlideTextObject = SlideObject & {
+  content: string
+  align: string
+  family: string
+  size: string
+  weight: string
+  color: string
+}
+
+export type Slide = {
+  objects: (SlideObject | SlideTextObject)[]
+}
+
+export type Slides = Slide[]
+
+export type File = {
+  slides: Slides
+}
