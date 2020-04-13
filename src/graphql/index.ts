c64bd19c4b8b6c6f5514e8dab335973acc73c39a
senpin <vkdnjdldnjsw@naver.com>
2020-04-13 13:28:13 +0900

    write resolvers automatically load code

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,15 +1,26 @@
 import { importSchema } from 'graphql-import'
-import user from './test2/test2.resolvers'
-import test from './test/test.resolvers'
-
+import require_all from 'require-all'
 import { makeExecutableSchema } from 'graphql-tools'
 import { GraphQLSchema } from 'graphql'
 
 const typeDefs = importSchema('./**/*.graphql')
-
+const controllers = require_all({
+  dirname: 'src/graphql',
+  filter: function (fileName) {
+    const parts = fileName.split('.')
+    if (parts[parts.length - 2] !== 'resolvers') return
+    return fileName
+  },
+})
+const modules = []
+for (const folder in controllers) {
+  for (const module in controllers[folder]) {
+    modules.push(controllers[folder][module].default)
+  }
+}
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs,
-  resolvers: [user, test],
+  resolvers: modules,
 })
 
 export default schema
